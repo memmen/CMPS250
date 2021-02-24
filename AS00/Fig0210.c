ab484ade0916081a71a8497e0487e9c0e456a50a
Brian S. Memmen <brian.memmen@gmail.com>
2021-02-24 16:50:01 -0500

    AS00

---
+++ AS00/Fig0210.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    const int limit = 100;
+    int num;
+    scanf("%d", &num);
+    if (num >= limit)
+    {
+        printf("high\n");
+    }
+    else
+    {
+        printf("low\n");
+    }
+    return 0;
+}
